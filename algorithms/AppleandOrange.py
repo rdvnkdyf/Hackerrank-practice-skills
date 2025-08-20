2c4274a75c7ff671858fdf278d10a487a2121ef7
rdvnkdyf <49274168+rdvnkdyf@users.noreply.github.com>
2025-08-20 17:38:35 +0300

    2008ahackerrank
    2008ahackerrank

---
+++ algorithms/AppleandOrange.py
@@ -0,0 +1,70 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apple_count = 0
+    orange_count = 0
+
+    
+    for apple_distance in apples:
+        
+        apple_position = a + apple_distance
+        
+        
+        if s <= apple_position <= t:
+            apple_count += 1
+
+    
+    for orange_distance in oranges:
+        
+        orange_position = b + orange_distance
+
+        
+        if s <= orange_position <= t:
+            orange_count += 1
+
+    
+    print(apple_count)
+    print(orange_count)
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
---
+++ algorithms/GradingStudents.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    
+    rounded_grades = []
+
+    
+    for grade in grades:
+        if grade < 38:
+            rounded_grades.append(grade)
+            continue
+
+        
+        remainder = grade % 5
+
+        
+        difference = 5 - remainder
+
+        
+        if difference < 3:
+            
+            new_grade = grade + difference
+            rounded_grades.append(new_grade)
+        else:
+            
+            rounded_grades.append(grade)
+
+    
+    return rounded_grades
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
