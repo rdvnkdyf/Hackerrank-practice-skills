67b88efaf1c307544cb92dae08ad8971bb876fa3
rdvnkdyf <49274168+rdvnkdyf@users.noreply.github.com>
2025-08-24 23:18:11 +0300

    2408hackerrank3
    2408hackerrank3

---
+++ Data Structures/Comparetwolinkedlists.py
@@ -0,0 +1,94 @@
+#!/bin/python3
+
+import os
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the compare_lists function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def compare_lists(llist1, llist2):
+    # Traverse both lists simultaneously
+    current1 = llist1
+    current2 = llist2
+    
+    while current1 is not None and current2 is not None:
+        # If the data values are not equal, the lists are not equal
+        if current1.data != current2.data:
+            return 0
+        
+        # Move to the next nodes
+        current1 = current1.next
+        current2 = current2.next
+    
+    # After the loop, check if both lists ended at the same time
+    # This means they had the same length and all data was equal
+    if current1 is None and current2 is None:
+        return 1
+    else:
+        # If one list is longer than the other, they are not equal
+        return 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        result = compare_lists(llist1.head, llist2.head)
+
+        fptr.write(str(int(result)) + '\n')
+
+    fptr.close()
