f1f8e17db9b6604fe4c33e52e4c44ce3efb6a400
rdvnkdyf <49274168+rdvnkdyf@users.noreply.github.com>
2025-08-27 20:58:01 +0300

    2708hackerrank1
    2708hackerrank1

---
+++ Data Structures/GetNodeValue.py
@@ -0,0 +1,96 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'getNode' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER positionFromTail
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def getNode(llist, positionFromTail):
+    # Initialize two pointers, 'fast' and 'slow', at the head of the list.
+    fast = llist
+    slow = llist
+
+    # Move the 'fast' pointer forward by 'positionFromTail' steps.
+    # This creates the desired gap between the two pointers.
+    for _ in range(positionFromTail):
+        fast = fast.next
+
+    # Move both pointers forward one step at a time until 'fast' reaches the end of the list.
+    # When 'fast' is at the last node, 'slow' will be at the desired position.
+    while fast.next is not None:
+        fast = fast.next
+        slow = slow.next
+
+    # Return the data value of the node pointed to by 'slow'.
+    return slow.data
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        position = int(input())
+
+        result = getNode(llist.head, position)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
